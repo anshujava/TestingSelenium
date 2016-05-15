9912f81ac2d802fda7a82f5d18f49a31051534eb
anshu <anshu@192.168.0.2>
2016-05-15 19:01:02 +0530

    added

---
+++ Selenum_Project/src/practice/PrimeNo.java
@@ -0,0 +1,22 @@
+package practice;
+
+public class PrimeNo {
+//Prime number in Java: 
+//	Prime number is a number that is greater than 1 and divided by 1 or itself. 
+//	In other words, prime numbers can't be divided by other numbers than itself or 1. 
+//	For example 2, 3, 5, 7, 11, 13, 17.... are the prime numbers.
+	 public static void main(String args[]){  
+		  int i,m=0,flag=0;    
+		  int n=17;//it is the number to be checked  
+		  m=n/2;    
+		  for(i=2;i<=m;i++){    
+		   if(n%i==0){    
+		   System.out.println("Number is not prime");    
+		   flag=1;    
+		   break;    
+		   }    
+		  }    
+		  if(flag==0)    
+		  System.out.println("Number is prime");    
+		}  
+		}  
--- Selenum_Project/src/practice/anshuSQL.sql
+++ Selenum_Project/src/practice/anshuSQL.sql
@@ -1,2 +1,6 @@
-select salary from employee A where 5 > (select count(b.salary) from employee B  where b.salary>a.salary )
-select salary from employee A where 2 = (select count(salary) from employee B  where b.salary>=a.salary )
+select salary from emptable A where 5 > (select count(b.salary) from emptable B  where b.salary>a.salary )
+
+
+select salary from emptable A where 2 = (select count(salary) from emptable B  where b.salary>=a.salary )
+
+SELECT * FROM emptable ORDER BY tdate DESC LIMIT 1,5;
