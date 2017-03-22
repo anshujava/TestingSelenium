0894f7e014df6734678179bf24ee5e30bb413414
anshu <anshu@192.168.0.2>
2017-03-22 13:34:49 +0530

    updated

--- Selenum_Project/src/SelenieumProject/SearchSuggestion.java
+++ Selenum_Project/src/SelenieumProject/SearchSuggestion.java
@@ -1,5 +1,9 @@
 package SelenieumProject;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,7 +17,8 @@ WebDriver driver;
  
  @BeforeTest
  public void start(){
-   driver = new FirefoxDriver(); 
+	System.setProperty("webdriver.gecko.driver", "F:/git/TestingSelenium/Selenum_Project/src/lib/geckodriver.exe");
+	driver = new FirefoxDriver(); 
    driver.get("http://www.google.com");
    driver.manage().window().maximize();
  }
@@ -21,18 +26,44 @@ WebDriver driver;
  @Test
   public void SearchSuggestion(){
 	 try{
-	 driver.findElement(By.name("q")).sendKeys("stackoverflow");  
+	 List<String> ls = new ArrayList<String>();
+	 String s ="how to cook rice";
+	 driver.findElement(By.name("q")).sendKeys(s);  
 	 Thread.sleep(5000);
-//	 driver.findElement(By.xpath("//ul[@role='listbox']//div[.='stackoverflow jobs']")).click();
 	 List<WebElement> options = driver.findElements(By.xpath("//ul[@role='listbox']"));
 	 for(int i=0;i<options.size();i++){
-         System.out.println("all list::"+options.get(i).getText());
+      String sar[]=options.get(i).getText().split("\\n");
+      for(int j =0;j<sar.length;j++){
+    	
+    	ls.add(sar[j]);
+      }
+     
      }
-	 driver.quit();
+	 System.out.println(ls);
+	 Object str = findClosestMatch(ls,s);
+	 System.out.println("match=="+str);
+	 driver.findElement(By.xpath("//*[text()='"+str+"']")).click();
+	 Thread.sleep(3000);
+//	 driver.quit();
 	 }catch(InterruptedException iex){
 		 System.out.println("Exception in::"+iex.getMessage());
 		 
 	 }
 }
-  
+ /*
+  * http://stackoverflow.com/questions/12244810/string-approximation-fetching-the-nearest-matched-string-from-dictionary
+  * string best matching java example
+  */
+ public static Object findClosestMatch(Collection<String> collection, Object target) {
+     int distance = Integer.MAX_VALUE;
+     Object closest = null;
+     for (Object compareObject : collection) {
+         int currentDistance = StringUtils.getLevenshteinDistance(compareObject.toString(), target.toString());
+         if(currentDistance < distance) {
+             distance = currentDistance;
+             closest = compareObject;
+         }
+     }
+     return closest;
+ }
 }
